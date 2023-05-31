fb6f42acee5cc417261bd3e6492883e6e9abb342
Luis Henrique Bueno <user.username>
2023-05-31 15:26:31 -0300

    ALLOWED_HOSTS

--- cais_tech/settings.py
+++ cais_tech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1s8a3v=t_-w4egk+ec2!3-prd+s9sldf8karrpo#s4i@ae12a2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
